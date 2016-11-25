4c96891313da1945e33ac4a8922c19ab6eacd963
gstanescu <georgestanescu90@gmail.com>
2016-11-25 17:28:55 +0200

    get posts from server and display them in a list group

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PostsService } from "./posts.service";
+import { Post } from "./post";
 
 @Component({
   selector: 'app-posts',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent implements OnInit {
+  posts: Post[];
+  isLoading: boolean = true;
 
-  constructor() { }
+  constructor(private _service: PostsService) { }
 
   ngOnInit() {
+    this._service.getPosts()
+      .subscribe(posts => {
+        this.posts = posts;
+      },
+      null,
+      () => {
+        this.isLoading = false;
+      });
   }
 
 }
---
+++ src/app/posts/posts.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from "@angular/core";
+import { Post } from "./post";
+import { Http, Response } from "@angular/http";
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
+
+@Injectable()
+export class PostsService {
+  private _url = "http://jsonplaceholder.typicode.com/posts";
+
+  constructor(private _http: Http) {
+
+  }
+  
+  getPosts(): Observable<Post []> {
+    return this._http.get(this._url)
+      .map(response => response.json())
+      .catch(this.handleError);
+  }
+
+  private extractData(res: Response) {
+    return res.json() || {};
+  }
+
+  private handleError(error: Response | any) {
+    // In a real world app, we might use a remote logging infrastructure
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
+}
