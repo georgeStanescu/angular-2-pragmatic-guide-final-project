92fff565e328c8a3d8994bed412603f7cae2005d
gstanescu <georgestanescu90@gmail.com>
2016-11-25 12:57:56 +0200

    making a post requst to save a new user

--- src/app/users/user-editor.component.ts
+++ src/app/users/user-editor.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from "@angular/forms";
+import { Router } from "@angular/router";
 import { CommonValidators, IFormComponent } from "../common";
+import { UsersService } from "./users.service";
+import { IUser } from "./user";
 
 @Component({
   selector: 'app-user-editor',
@@ -10,7 +13,7 @@ import { CommonValidators, IFormComponent } from "../common";
 export class UserEditorComponent implements IFormComponent, OnInit {
   formGroup: FormGroup;
 
-  constructor(formBuilder: FormBuilder) {
+  constructor(formBuilder: FormBuilder, private _service: UsersService, private _router: Router) {
     this.formGroup = formBuilder.group({
       nameControl: ['', Validators.required],
       emailControl: ['', Validators.compose([Validators.required, CommonValidators.emailInvalid])],
@@ -30,7 +33,18 @@ export class UserEditorComponent implements IFormComponent, OnInit {
   }
 
   addUser() {
-    console.log(this.formGroup.value);
+    let user = <IUser>{ name: this.formGroup.get("nameControl").value, email: this.formGroup.get("emailControl").value };
+    
+    this._service.saveUser(user)
+      .subscribe((savedUser) => {
+        console.log(savedUser);
+      },
+      null,
+      () => {
+        this.formGroup.markAsPristine();
+        this._router.navigate(["/users"]);
+      }
+    );
   }
 
 }
--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { IUser } from "./user";
-import { Http, Response } from "@angular/http";
+import { Http, Response, Headers, RequestOptions } from "@angular/http";
 import { Observable } from "rxjs/Observable";
 import "rxjs/add/operator/map";
 import "rxjs/add/operator/catch";
@@ -12,14 +12,37 @@ export class UsersService {
   constructor(private _http: Http) {
   }
 
+  saveUser(user: IUser): Observable<IUser> {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+
+    return this._http
+      .post(this._usersUrl, JSON.stringify(user), options)
+      .map(this.extractData)
+      .catch(this.handleError);
+  }
+
   getUsers(): Observable<IUser []> {
     return this._http.get(this._usersUrl)
       .map(response => response.json())
       .catch(this.handleError);
   }
 
-  private handleError(error: Response) {
-    console.error(error);
-    return Observable.throw(error.json().error || "Server error");
+  private extractData(res: Response) {
+    return res.json() || { };
+  }
+
+  private handleError (error: Response | any) {
+  // In a real world app, we might use a remote logging infrastructure
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
   }
 }
